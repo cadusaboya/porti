4f0060895a3b19c1544279d0520c9eccd2d2296a
cadusaboya <carlosepsaboya@gmail.com>
2024-10-08 16:25:14 -0300

    removed CopilotProvider from App.tsx

--- react-native/App.tsx
+++ react-native/App.tsx
@@ -4,16 +4,13 @@ import AppNavigator from './app/AppNavigator';
 import { registerRootComponent } from 'expo';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AuthProvider } from './context/AuthContext'; // Update the file path here
-import { CopilotProvider } from "react-native-copilot";
 
 export default function App() {
   return (
     <AuthProvider>
       <SafeAreaProvider>
         <NavigationContainer>
-          <CopilotProvider>
             <AppNavigator />
-          </CopilotProvider>
         </NavigationContainer>
       </SafeAreaProvider>
     </AuthProvider>
